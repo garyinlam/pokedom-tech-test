3c16a4a7d480a849ea52e227e6ad8d87200b4949
Gar-Yin Lam <garyin.lam@gmail.com>
2022-08-23 14:37:55 +0100

    capitalise first letter of name

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 import {pokemonArray} from './data/pokemon.js';
 
-const cardContianer = document.querySelector(".card-container");
+const cardContainer = document.querySelector(".card-container");
 
 pokemonArray.forEach((pokemon) => {
+  //pokemon object to HTML
+  //get object properties
   const {name, id, types, sprite} = pokemon;
+  //capitalise first letter of name
+  const nameCap = name.slice(0,1).toUpperCase()+name.slice(1);
+  //convert types from array to string
   const typesString = types.join(" & ");
+  //create html to insert
   const htmlString = `
   <div class="card">
-    <img src="${sprite}" alt="sprite of ${name}" class="card__image">
+    <img src="${sprite}" alt="sprite of ${nameCap}" class="card__image">
     <div class="card__content">
-      <h2 class="card__heading">${name}</h2>
-      <p class="card__text">${name} (#${id}) is a ${typesString} type pokemon.</p>
+      <h2 class="card__heading">${nameCap}</h2>
+      <p class="card__text">${nameCap} (#${id}) is a ${typesString} type pokemon.</p>
     </div>
   </div>
   `
-  cardContianer.innerHTML += htmlString;
+  //insert html into card-container at end
+  cardContainer.innerHTML += htmlString;
 });
